8fde97363d946afc9186062d933e61c90bf31d54
Dennis Li <dennis.li.92@gmail.com>
2016-12-29 13:16:35 -0800

    make alternative arch -- ListViewUncontrolled

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import * as ReactDOM from "react-dom";
 
 import { ListView } from "./components/ListView";
 import { ListViewSlow1 } from "./components/ListViewSlow1";
+import { ListViewUncontrolled } from "./components/ListViewUncontrolled";
 
 ReactDOM.render(
-    <ListView />,
+    //<ListView />,
 
+    <ListViewUncontrolled />,
     // <ListViewSlow1 />,
 
     document.getElementById("app-container")
