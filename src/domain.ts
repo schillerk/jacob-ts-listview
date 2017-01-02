579a60291bd89340818d52ede53a9f10637348c6
Jacob Cole <tmad4000@gmail.com>
2017-01-01 18:17:00 -0800

    Cleanup

--- src/domain.ts
+++ src/domain.ts
@@ -5,7 +5,7 @@ export interface Gestalt {
     gestaltId: string;
     text: string;
     relatedIds: string[];
-    instanceIds:{[instanceId:string]: boolean}
+    instanceAndVisibleNubIds: {[instanceId:string]: boolean}
 }
 
 export interface GestaltCollection {
@@ -18,13 +18,11 @@ export interface GestaltInstance {
     parentGestaltInstanceId: string
     shouldUpdate: boolean 
 
-    // instanceId: string;
     gestaltId: string;
-    // expanded: boolean;
     // childInstances: GestaltInstance[];
 }
 
-export function createGestaltInstance(gestalt: Gestalt){
+export function createGestaltInstance(gestalt: Gestalt) {
     // var newInstance : GestaltInstance = {
     //     instanceId: Util.genGUID(),
     //     childInstances: [],
